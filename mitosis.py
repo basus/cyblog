c589ba87147d9194c88d5d442572f81da780b402
Shrutarshi Basu <shr@basushr.net>
2009-09-19 20:32:35 -0400

    Moved to object oriented structure

--- mitosis.py
+++ mitosis.py
@@ -6,34 +6,40 @@ import os, yaml, shutil, pickle, time
 #             print filename
 #         print "DONE"
 
-try:
-    tsfile = open('timestamp')
-    timestamp = pickle.load(tsfile)
-    tsfile.close()
-except:
-    timestamp = 0
+class mitosis:
 
-output = 'mitosis'
-outdir = os.getcwd() + '/' + output
-
-for root, dir, files in os.walk('.'):
-    if root.find('/.') == -1 and root.find('./'+output) == -1:
-        print '\n'
-        current_outdir = outdir + root[1:] + '/'
+    def __init__(self):
+        self.output = 'mitosis'
+        self.outdir = os.getcwd() + '/' + self.output
         try:
-            os.mkdir(current_outdir)
+            tsfile = open('timestamp')
+            self.timestamp = pickle.load(tsfile)
+            tsfile.close()
         except:
-            pass
-        for filename in files:
-            modify_time = os.path.getmtime(root +'/'+filename.lstrip('.'))
-            print modify_time, timestamp
-            if modify_time  > timestamp:
-                if filename.endswith('.markdown') or filename.endswith('.mkdwn'):
-                    fl = open(current_outdir + filename, 'w')
-                    fl.close()
-                else:
-                    print root+filename
-                    shutil.copy(root+'/'+filename.lstrip('.'), current_outdir+filename)
+            self.timestamp = 0
+
+    def process(self):
+        for root, dir, files in os.walk('.'):
+            if root.find('/.') == -1 and root.find('./'+self.output) == -1:
+                print '\n'
+                current_outdir = self.outdir + root[1:] + '/'
+                try:
+                    os.mkdir(current_outdir)
+                except:
+                    pass
+                for filename in files:
+                    modify_time = os.path.getmtime(root +'/'+filename.lstrip('.'))
+                    print modify_time, self.timestamp
+                    if modify_time  > self.timestamp and filename[-1] != '~':
+                        if filename.endswith('.markdown') or filename.endswith('.mkdwn'):
+                            fl = open(current_outdir + filename, 'w')
+                            fl.close()
+                        else:
+                            print root+filename
+                            shutil.copy(root+'/'+filename.lstrip('.'), current_outdir+filename)
+
+converter = mitosis()
+converter.process()
 
 tsfile = open('timestamp', 'w')
 pickle.dump(time.time()+10, tsfile)
